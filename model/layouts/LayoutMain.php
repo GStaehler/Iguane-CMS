d0f0d055350d8055a3484ff8df062930c9ec99ba
gauthier.staehler <gauthier.staehler@uha.fr>
2019-04-01 13:46:30 +0200

    design and database updated

--- model/layouts/LayoutMain.php
+++ model/layouts/LayoutMain.php
@@ -6,10 +6,10 @@ class LayoutMain
     public function integrate($page)
     {
         echo "<div id=\"layoutMain\" class=\"col-8 container\">";
-        $text = new Text;
-        $text->integrate(1, $page);
         $img = new Image;
         $img->integrate(1, $page);
+        $text = new Text;
+        $text->integrate(1, $page);
         echo "</div>";
     }
     
--- sql/filrougephp.sql
+++ sql/filrougephp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le :  lun. 01 avr. 2019 à 11:26
+-- Généré le :  lun. 01 avr. 2019 à 13:45
 -- Version du serveur :  10.1.38-MariaDB
 -- Version de PHP :  7.3.2
 
@@ -45,15 +45,14 @@ INSERT INTO `element` (`id`, `type`, `layout`, `content`, `page`) VALUES
 (77, 5, 1, 'Ceci est une zone de texte !', 0),
 (80, 6, 3, 'http://www.taklom360.com/images/placeholders/1920x1200-1.jpg', 0),
 (81, 3, 1, 'Title', 0),
-(82, 5, 1, 'Lorem Ipsum etc', 0),
+(82, 5, 1, 'Praesent quis diam molestie nunc vehicula condimentum vel id augue. Nunc dapibus vestibulum mauris, placerat ullamcorper odio dictum in. Phasellus quis diam nec felis ultrices mattis id id quam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam porttitor, lorem ac mattis consequat, magna ex auctor libero, sit amet sodales ante ipsum ut eros. Nulla aliquet, odio id dictum gravida, enim risus vulputate erat, a ultricies est dolor a orci. Quisque tempus quam eget nulla pellentesque ornare. Vivamus a viverra est. Donec sit amet est non nulla sollicitudin dignissim eu accumsan dolor. Fusce nunc augue, ullamcorper sed augue at, rhoncus congue sem. Proin aliquet faucibus nisl nec vestibulum. Suspendisse aliquet sem eu congue pellentesque. ', 0),
 (83, 5, 2, 'Zone de texte dans le layout aside !', 0),
-(84, 5, 2, 'Un autre dans aside', 0),
-(85, 5, 1, 'Un dans main', 0),
-(86, 5, 2, 'Je fais un texte dans le layout aside', 0),
-(87, 5, 1, 'Et un texte dans le layout main', 0),
 (96, 2, 1, 'Fil Rouge', 0),
-(101, 5, 1, 'Test', 3),
-(103, 5, 2, 'Test', 10);
+(101, 5, 1, 'Hey (brrrr)\r\nOuh, on va t\'shooter dans la galerie\r\nChoppa vidé dans la galerie\r\nNah, nah, nah wasn\'t me\r\nNah, nah, nah wasn\'t me (no)\r\nGucci, Fendi dans ma galerie\r\nPolice m\'appelle, j\'réponds pas, je nie\r\nNah, nah, nah wasn\'t me\r\nNah, nah, nah wasn\'t me', 3),
+(103, 5, 2, 'Test', 10),
+(109, 7, 1, 'http://emi.ecpad.fr/wp-content/themes/invictus_3.3.1/images/dummy-image.jpg', 0),
+(110, 7, 2, 'https://www.raprnb.com/wp-content/uploads/2019/02/Hamza-Paradise.png', 3),
+(111, 5, 2, 'Nouveaux ennemis, nouvelles armes\r\nT\'aimes pas la paix c\'est qu\'t\'as pas fait la guerre guerre guerre\r\nL\'oseille et la mif, éclairent mon âme\r\nJ\'refais la peinture du frigo en bleu jaune vert\r\nOn veut notre coin d\'herbe on veut la planète Terre\r\nOn veut le monde Chico\r\nT\'inquiète, on rendra tout ce qu\'on acquiert\r\nOn veut le monde Chico', 0);
 
 -- --------------------------------------------------------
 
@@ -115,7 +114,8 @@ INSERT INTO `type` (`id`, `name`) VALUES
 (3, 'title'),
 (4, 'footer'),
 (5, 'textzone'),
-(6, 'background');
+(6, 'background'),
+(7, 'image');
 
 --
 -- Index pour les tables déchargées
@@ -156,7 +156,7 @@ ALTER TABLE `type`
 -- AUTO_INCREMENT pour la table `element`
 --
 ALTER TABLE `element`
-  MODIFY `id` mediumint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=109;
+  MODIFY `id` mediumint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=112;
 
 --
 -- AUTO_INCREMENT pour la table `layout`
@@ -174,7 +174,7 @@ ALTER TABLE `page`
 -- AUTO_INCREMENT pour la table `type`
 --
 ALTER TABLE `type`
-  MODIFY `id` mediumint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` mediumint(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
