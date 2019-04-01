56d179ee4975157066740df8a71a275fbc6d2185
gauthier.staehler <gauthier.staehler@uha.fr>
2019-04-01 11:53:03 +0200

    Image class created

---
+++ model/elements/Image.php
@@ -0,0 +1,17 @@
+<?php
+
+class Image extends Element
+{
+
+    private $isMultiple = true;
+
+    /* public function integrate($layout, $page)
+    {
+        global $bdd;
+        parent::getDatabase();
+        $req = $bdd->query('SELECT element.content, element.layout FROM element INNER JOIN type ON element.type = type.id WHERE element.type = 7 AND element.layout = ' . $layout . ' AND element.page = ' . $page);
+        while ($data = $req->fetch()) {
+            echo "<img class=\"imageElement\" src=\"" . $data['content'] . "\">";
+        }
+    } */
+}
