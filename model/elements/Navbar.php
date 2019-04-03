cb0c8aed563d4f569df83cd8742080ffa0b79495
gauthier.staehler <gauthier.staehler@uha.fr>
2019-04-03 14:05:44 +0200

    design

--- model/elements/Navbar.php
+++ model/elements/Navbar.php
@@ -11,7 +11,7 @@ class Navbar extends Element
         parent::getDatabase();
         $req = $bdd->query('SELECT element.content FROM element INNER JOIN type ON element.type = type.id WHERE element.type = 2');
         while ($data = $req->fetch()) {
-            echo "<header><nav class=\"navbar navbar-dark bg-dark\">
+            echo "<header><nav class=\"navbar navbar-dark bg-dark\" style=\"background-color: #212529 !important;\">
                 <a class=\"navbar-brand\" href=\"/\">" . $data['content'] . "</a>
                 <ul class=\"navbar-nav d-flex flex-row justify-content-start\" style=\"position: absolute; left: 132px;\">";
             $bdd2 = new PDO('mysql:host=127.0.0.1;dbname=filrougephp;charset=utf8', 'root', '');
