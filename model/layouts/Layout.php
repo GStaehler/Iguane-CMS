541685bb13da2df312da341b697dc9af2aa516c0
gauthier.staehler <gauthier.staehler@uha.fr>
2019-04-15 09:42:09 +0200

    cleaned

--- model/layouts/Layout.php
+++ model/layouts/Layout.php
@@ -2,13 +2,10 @@
 
 abstract class Layout
 {
-
-    private $bdd;
     
     public function getDatabase(): void
     {
-        global $bdd;
-        $bdd = new PDO('mysql:host=127.0.0.1;dbname=iguane;charset=utf8', 'root', '');
+        require("model/database.php");
     }
 
 }
