2cf7557dced926a7946cd442a5630342cb52a27f
gauthier.staehler <gauthier.staehler@uha.fr>
2019-04-15 12:57:25 +0200

    ideas about theme

--- model/elements/Element.php
+++ model/elements/Element.php
@@ -3,9 +3,16 @@
 abstract class Element
 {
     
+    // private $theme = "light";
+    
     public function getDatabase(): void
     {
         require("model/database.php");
     }
+    
+    /* public function setTheme($theme): void
+    {
+        $this->theme = $theme;
+    } */
 
 }
