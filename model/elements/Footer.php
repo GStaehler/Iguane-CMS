4a6bf6a01335108b619cbfb2dabb2a0d1bfcfb0b
gauthier.staehler <gauthier.staehler@uha.fr>
2019-04-17 10:00:57 +0200

    footer cleaned

--- model/elements/Footer.php
+++ model/elements/Footer.php
@@ -22,7 +22,7 @@ class Footer extends Element
         $this->getNvf();
         $req = $bdd->query('SELECT element.content FROM element INNER JOIN type ON element.type = type.id WHERE element.type = 4');
         while ($data = $req->fetch()) {
-            echo "<footer class=\"" . $this->nvf . "\"><div class=\"text-center\"><p class=\"text-muted\">&copy; " . date("Y") . " - " . $data['content'] . "</p></div></footer>";
+            echo "<footer class=\"text-center " . $this->nvf . "\"><p class=\"text-muted\">&copy; " . date("Y") . " - " . $data['content'] . "</p></footer>";
         }
     }
 }
