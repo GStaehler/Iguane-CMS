62569dd5640e13b4c715359a2f6afcec1cf3aa0b
gauthier.staehler <gauthier.staehler@uha.fr>
2019-05-17 16:36:10 +0200

    tests on installation

--- model/database.php
+++ model/database.php
@@ -1,3 +1,13 @@
 <?php
 
-$bdd = new PDO('mysql:host=127.0.0.1;dbname=iguane;charset=utf8', 'root', '');
+try {
+$bdd = new PDO('mysql:host=127.0.0.1;dbname=iguane;charset=utf8', 'root', '');    
+}
+
+catch (Exception $e) {
+    echo "Database does not exists !";
+}
+
+if (!$bdd) {
+    throw new Exception('Database does not exists !');
+}
