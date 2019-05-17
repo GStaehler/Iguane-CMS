3bd6a3f2f3a76ece75ecbc76db5de627ad03e626
GStaehler <gauthier.staehler@outlook.com>
2019-05-17 22:20:11 +0200

    tests on installation

--- model/database.php
+++ model/database.php
@@ -12,5 +12,26 @@ catch (Exception $e) {
 	require_once("vendor/bootstrap-4.3.1-dist/css/bootstrap.min.css");
 	echo "</style>";
 	echo "<link href=\"https://fonts.googleapis.com/css?family=Indie+Flower\" rel=\"stylesheet\">";
-    echo "<div class=\"container text-center\" style=\"margin-top: 80px;\"><h1 style=\"font-family: 'Indie Flower';\">Iguane CMS</h1><div class=\"mb-4 lead\" style=\"font-family: 'Noto Sans', sans-serif;\">Installation</div><div class=\"mb-4 alert alert-danger\">Database doesn't exist !</div></div>";
+    echo "<div class=\"container text-center\" style=\"margin-top: 80px;\"><h1 style=\"font-family: 'Indie Flower';\">Iguane CMS</h1><div class=\"mb-4 lead\" style=\"font-family: 'Noto Sans', sans-serif;\">Installation</div><br><div class=\"mb-4 alert alert-danger\">Database doesn't exist !</div>";
+	
+	echo "<form action=\"\" method=\"post\">";
+	echo "<br><small>Create database with username=\"root\" and password=\"\"</small><br>";
+	echo "<br><input type=\"submit\" name=\"createDb\" class=\"btn btn-outline-success btn-lg\" value=\"Create Database\">";
+	echo "</form>";
+
+	if (isset($_POST['createDb'])) {
+
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+
+		$conn = new PDO("mysql:host=$servername", $username, $password);
+		$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		$sql = "CREATE DATABASE iguane";
+		$conn->exec($sql);
+		echo "<div class=\"mb-4\">Database created successfully !</div>";
+		$conn = null;
+	}
+	
+	echo "</div>";
 }
